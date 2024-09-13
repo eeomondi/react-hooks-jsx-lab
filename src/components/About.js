c1f83caedaa32938b37f6fa6c5bee1107abef28d
Elvis <elvis.omondi@student.moringaschool.com>
2024-09-13 19:56:57 +0300

    commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,11 @@
 import React from "react";
-import image from "./my-image.jpg"; // Update this to the actual image file name and path
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>This is a short paragraph about me. Feel free to customize it.</p>
-      <img src={image} alt="I made this" />
+      <img src="https://via.placeholder.com/150" alt="Placeholder" />
     </div>
   );
 }
